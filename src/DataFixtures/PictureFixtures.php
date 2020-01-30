d0f753cf4f54497ec2e5c3a1f1847b25daea71fe
kantum21 <quentinsporn@gmail.com>
2020-01-30 16:11:21 +0100

    Create Picture entity & features

---
+++ src/DataFixtures/PictureFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Picture;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class PictureFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $picture = new Picture();
+        $picture->setPicture('defaultTrick.jpg');
+        $manager->persist($picture);
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Picture.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PictureRepository")
+ */
+class Picture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $picture;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+}
