2cff9f36179d6836c1d174627bfe976c0b0a276f
kantum21 <quentinsporn@gmail.com>
2020-04-04 17:15:53 +0200

    Edit trick

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -39,4 +39,47 @@ class VideoController extends AbstractController
             'videoForm' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("videos/edit/{id}", name="video_edit")
+     * @param Request $request
+     * @param Video $video
+     * @param EntityManagerInterface $entityManager
+     * @return RedirectResponse|Response
+     */
+    public function edit(Request $request, Video $video, EntityManagerInterface $entityManager)
+    {
+        $form = $this->createForm(VideoFormType::class, $video);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            /** @var Video $video */
+            $video = $form->getData();
+            $entityManager->persist($video);
+            $entityManager->flush();
+            $this->addFlash('success', 'Video saved !');
+
+            return $this->redirectToRoute('app_homepage');
+        }
+
+        return $this->render('video/edit.html.twig', [
+            'videoForm' => $form->createView(),
+            'video' => $video
+        ]);
+    }
+
+    /**
+     * @Route("videos/delete/{id}", name="video_delete")
+     * @param Video $video
+     * @param EntityManagerInterface $entityManager
+     * @return RedirectResponse
+     */
+    public function delete(Video $video, EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($video);
+        $entityManager->flush();
+        $this->addFlash('success', 'Video deleted !');
+        return $this->redirectToRoute('app_homepage');
+    }
 }
